ac16d7ee4863b495ab0a783800f90cfd63e4519f
Ivan Esparza <ivan-esparza@morsum.co>
2021-12-07 18:29:29 -0800

    Day 7

---
+++ 07/part2/crab_align_sum.kts
@@ -0,0 +1,37 @@
+import java.io.File
+import java.lang.IllegalArgumentException
+import kotlin.system.exitProcess
+
+val input = if(args.contains("-i")) args[1 + args.indexOf("-i")] else throw IllegalArgumentException("Specifile a file with -i")
+
+val crabs = mutableListOf<Int>()
+var result = 0
+
+// File is one line long
+File(input).forEachLine { line ->
+    crabs.addAll(line.split(',').map { it.toInt() })
+}
+var maxX = 0
+var minX = Int.MAX_VALUE
+val crabMap = mutableMapOf<Int, Int>()
+
+crabs.forEach { crab ->
+    maxX = Math.max(crab, maxX)
+    minX = Math.min(crab, minX)
+    crabMap.put(crab, crabMap[crab]?.let { it + 1} ?: 1)
+}
+
+var minCost = Int.MAX_VALUE
+
+for (pos in minX .. maxX) {
+    var cost = 0
+    crabMap.forEach { entry ->
+        val dx = Math.abs(entry.key - pos)
+        val fuel = dx * (dx + 1) / 2
+        cost += fuel * entry.value
+    }
+    minCost = Math.min(cost, minCost)
+}
+
+
+println("Min fuel cost of crab align is $minCost")
